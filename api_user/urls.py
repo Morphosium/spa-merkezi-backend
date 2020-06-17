f8756beb80589f447188fa898b3932700a1cb21e
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2020-06-17 17:20:28 +0300

    user endpoint informations added

--- api_user/urls.py
+++ api_user/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
-
+from rest_framework.urls import url
+from .views import loggedUserInformation
 urlpatterns = [
     # Your URLs...
-      url(r'^api-token-auth/', obtain_jwt_token),
+      url(r'^authorize/', obtain_jwt_token),
+      url(r'^informations/', loggedUserInformation.as_view())
 ]
+
+app_name = 'user_api'
--- api_user/views.py
+++ api_user/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView 
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .serializers import UserSerializer
 # Create your views here.
+
+
+
+class loggedUserInformation(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        if (request.user is not None):
+            userSerializer = UserSerializer(request.user)
+            return Response(userSerializer.data)
