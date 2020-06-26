30b58deef5f05689688010207d6f8c44bcb846b7
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2020-06-26 13:30:05 +0300

    randevu delete endpoint added

--- api_randevu/urls.py
+++ api_randevu/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework.urls import url
-from .views import createAppointment, fetchAuthorizedAppointments
+from .views import createAppointment, fetchAuthorizedAppointments, deleteAppointment
 
 urlpatterns = [
     # Your URLs...
       url(r'^appointment/create', createAppointment.as_view()),
       url(r'^appointment/fetch', fetchAuthorizedAppointments.as_view()),
-
+      path('appointment/<int:randevuId>/delete/', deleteAppointment.as_view())
 ]
 
 app_name = 'randevu_api'
--- api_randevu/views.py
+++ api_randevu/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.permissions import IsAuthenticated, AllowAny
+
+from utils.SubeIliskileri import iliskiVarMi
 from utils.arrayUtils import containsInDictionaryKey
 from utils.errorResponse import createErrorResponse
 from utils.appointments import makeArraySerializations
@@ -12,29 +14,29 @@ from .serializers import RandevuSerializer
 import traceback
 import sys
 from dateutil.parser import parse as dateUtilParse
+
+
 # Create your views here.
 
 class fetchAuthorizedAppointments(APIView):
-
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
         try:
-            return Response(makeArraySerializations(request.user))    
+            return Response(makeArraySerializations(request.user))
         except Exception as exception:
             traceback.print_exc()
-            return createErrorResponse(500,{"error": str(exception.__class__)})
+            return createErrorResponse(500, {"error": str(exception.__class__)})
 
 
 class createAppointment(APIView):
-
     permission_classes = [AllowAny]
 
     def post(self, request):
         try:
             data: dict = request.data
             if containsInDictionaryKey(
-                data, ["isim", "soyisim", "hizmetTipi", "subeId", "randevuTarih"]
+                    data, ["isim", "soyisim", "hizmetTipi", "subeId", "randevuTarih"]
             ):
                 # create RANDEVU(Appointment) instance - RANDEVU örneği yaratma
                 # find SUBE (Branch) - şube arama
@@ -42,7 +44,7 @@ class createAppointment(APIView):
                 if subeler.count() > 0:
                     sube = subeler[0]
                     tarih = dateUtilParse(data.get("randevuTarih"))
-                    #tarih = datetime.strptime(data.get("tarih"),"%Y-%m-%dT%H:%M:%S.%Z")
+                    # tarih = datetime.strptime(data.get("tarih"),"%Y-%m-%dT%H:%M:%S.%Z")
                     randevu = Randevu(
                         secili_sube=sube,
                         hizmet_turu=data.get("hizmetTipi"),
@@ -69,4 +71,24 @@ class createAppointment(APIView):
 
         except Exception as exception:
             traceback.print_exc()
-            return createErrorResponse(500,{"error": str(exception.__class__)})
+            return createErrorResponse(500, {"error": str(exception.__class__)})
+
+
+class deleteAppointment(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, randevuId):
+        if randevuId is None:
+            return createErrorResponse(400, {"message": "randevu Id not provided"})
+        else:
+            randevular = Randevu.objects.filter(id=randevuId)
+            randevu = randevular[0] if randevular.count() > 0 else None
+            if randevu is not None:
+                iliski = iliskiVarMi(request.user, randevu.secili_sube.id)
+                if iliski is not None:
+                    randevu.delete()
+                    return Response({"message": "Appointment deleted"}, 200)
+                else:
+                    return Response({"message": "You are unauthorized for this branch"}, 403)
+            else:
+                return Response({"message": "Randevu not found"}, 404)
