bfc0b6402cfec4d29cd48a95ce730c0496d9132a
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2020-07-01 16:22:52 +0300

    Appointment serializing

--- api_randevu/serializers.py
+++ api_randevu/serializers.py
@@ -1,24 +1,18 @@
 from cinarspa_models.models import Randevu
 from rest_framework import serializers
 
+from cinarspa_models.serializers import MusteriSerializer
+
 
 class RandevuSerializer(serializers.ModelSerializer):
     subeId = serializers.CharField(source='secili_sube.id')
     subeAdresi = serializers.CharField(source='secili_sube.sube_ismi')
 
-    musteri_isim = serializers.CharField(source='musteri.isim')
-    musteri_soyisim = serializers.CharField(source='musteri.isim')
-    musteri_email = serializers.CharField(source='musteri.email')
-    musteri_tel = serializers.CharField(source='musteri.tel')
-
+    musteri = MusteriSerializer()
     class Meta:
         model = Randevu
         fields = [
             'musteri',
-            'musteri_isim',
-            'musteri_soyisim',
-            'musteri_email',
-            'musteri_tel',
             'hizmet_turu',
             'tarih',
             'subeId',
