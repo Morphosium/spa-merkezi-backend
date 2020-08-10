eeec6cf277713960ecbffdc2ecd4c530ceebee7f
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2020-08-10 17:01:27 +0300

    Token exp time increased to 12 hours

--- cinarspa/settings.py
+++ cinarspa/settings.py
@@ -47,7 +47,7 @@ JWT_AUTH = {
 
     'JWT_VERIFY': True,
     'JWT_VERIFY_EXPIRATION': True,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=3000),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=12),
     'JWT_AUTH_HEADER_PREFIX': 'Bearer',
 
 }
